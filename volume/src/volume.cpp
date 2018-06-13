036caec6abcbf5238e180d33c7ee058a3ac93656
z80 <bashkirov.sergey@gmail.com>
2018-06-13 12:10:03 -0700

    Experimenting with noise and surface building. Right now it seems it is necessary to a) make noise mutex protected and b) make OctreeSplitPolicy result dependent to the distance to the camera and rebuild a mesh when camera moves out of predefined region.

--- volume/src/volume.cpp
+++ volume/src/volume.cpp
@@ -27,22 +27,26 @@ void Volume2::create()
     ChunkParameters parameters;
     parameters.sceneManager = smgr; // The SceneManager to use
     parameters.src = &ss; // The just created density source.
-    parameters.baseError = (Real)0.1; // The error of the highest LOD-level
-    parameters.errorMultiplicator = (Real)0.6; // The factor between each LOD-level (error = base
+    parameters.baseError = (Real)1.5; // The error of the highest LOD-level
+    parameters.errorMultiplicator = (Real)0.9; // The factor between each LOD-level (error = base
     // Error * errorMultiplicator * level)
     parameters.skirtFactor = (Real)0.7; // Controls how long the skirts are. The lower the numbe
     // r, the shorter the skirts are. This saves geometry. But if they are too short, cracks might o
     // ccure.
     parameters.scale = 10; // The displayed volume will be scaled by this factor.
-    parameters.maxScreenSpaceError = 30; // The screen space error controlling when the LOD-levels change.
+    parameters.maxScreenSpaceError = 3; // The screen space error controlling when the LOD-levels change.
+    parameters.async = false;
+    //parameters.createOctreeVisualization = true;
+    //parameters.createDualGridVisualization = true;
 
 
     volumeNode = smgr->getRootSceneNode()->createChildSceneNode();
 
     volume = OGRE_NEW Chunk();
-    volume->load( volumeNode, Vector3(-256), Vector3(256), 2, &parameters );
+    volume->load( volumeNode, Vector3(-10), Vector3(10), 5, &parameters );
     //volume->setDualGridVisible( true );
     //volume->setOctreeVisible( true );
+    //volume->setVolumeVisible( true );
 
     MaterialPtr mh = MaterialManager::getSingleton().getByName( "triplanarReference" );
     MaterialPtr ml = MaterialManager::getSingleton().getByName( "triplanarReference" );
