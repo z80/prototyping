e861222ae312f62e72c36ec2797465e95f0ed03f
z80 <bashkirov.sergey@gmail.com>
2018-06-13 09:20:32 -0700

    a

--- volume/src/volume.cpp
+++ volume/src/volume.cpp
@@ -27,20 +27,22 @@ void Volume2::create()
     ChunkParameters parameters;
     parameters.sceneManager = smgr; // The SceneManager to use
     parameters.src = &ss; // The just created density source.
-    parameters.baseError = (Real)0.18; // The error of the highest LOD-level
-    parameters.errorMultiplicator = (Real)0.9; // The factor between each LOD-level (error = base
+    parameters.baseError = (Real)0.1; // The error of the highest LOD-level
+    parameters.errorMultiplicator = (Real)0.6; // The factor between each LOD-level (error = base
     // Error * errorMultiplicator * level)
     parameters.skirtFactor = (Real)0.7; // Controls how long the skirts are. The lower the numbe
     // r, the shorter the skirts are. This saves geometry. But if they are too short, cracks might o
     // ccure.
     parameters.scale = 10; // The displayed volume will be scaled by this factor.
-    parameters.maxScreenSpaceError = 3; // The screen space error controlling when the LOD-levels change.
+    parameters.maxScreenSpaceError = 30; // The screen space error controlling when the LOD-levels change.
 
 
     volumeNode = smgr->getRootSceneNode()->createChildSceneNode();
 
     volume = OGRE_NEW Chunk();
     volume->load( volumeNode, Vector3(-256), Vector3(256), 2, &parameters );
+    //volume->setDualGridVisible( true );
+    //volume->setOctreeVisible( true );
 
     MaterialPtr mh = MaterialManager::getSingleton().getByName( "Examples/CloudySky" );
     MaterialPtr ml = MaterialManager::getSingleton().getByName( "Examples/CloudySky" );
