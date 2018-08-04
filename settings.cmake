3db80f045f84b6ececb888e2974142d62914374f
z80 <bashkirov.sergey@gmail.com>
2018-08-03 23:54:20 -0700

    a

--- settings.cmake
+++ settings.cmake
@@ -1,5 +1,5 @@
 
-set( OGRE_HOME "~/projects/ogre3d/ogre3d/install" )
+set( OGRE_HOME "~/projects/noise.git/ogre3d/install" )
 #set( OGRE_HOME "~/projects/ogre3d/ogre3d/install.debug" )
 #set( OGRE_HOME "~/projects/noise.git/ogre3d/install.debug" )
 
