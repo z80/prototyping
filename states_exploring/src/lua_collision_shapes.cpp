1d1699ea9b0153c249c2bf5c3640f3447cdad56c
sbashkirov <sergey.bashkirov@sony.com>
2018-08-27 19:03:21 -0700

    a

--- states_exploring/src/lua_collision_shapes.cpp
+++ states_exploring/src/lua_collision_shapes.cpp
@@ -1,10 +1,11 @@
 
+#include "lua_collision_shapes.h"
 #include "btBulletCollisionCommon.h"
 #include "config_reader.h"
 #include "lua.hpp"
 
-static const char LIB_NAME[]           = "core";
-static const char BULLET_META_T_NAME[] = "BT";
+static const char LIB_NAME[]                = "core";
+static const char SIMPLESHAPE_META_T_NAME[] = "SS";
 
 class ShapeSingle
 {
@@ -32,7 +33,7 @@ static int lua_btSphereCreate( lua_State * L )
 {
     const int top = lua_gettop( L );
     // Read parameters.
-    const btScalar r = static_cast<btScalar>( lua_tostring( L, 1 ) );
+    const btScalar r = static_cast<btScalar>( lua_tonumber( L, 1 ) );
 
     ShapeSingle * s = new ShapeSingle();
     s->s = new btSphereShape( r );
@@ -55,9 +56,9 @@ static int lua_btSphereCreate( lua_State * L )
 static int lua_btBoxCreate( lua_State * L )
 {
     // Read parameters.
-    const btScalar x = static_cast<btScalar>( lua_tostring( L, 1 ) );
-    const btScalar y = static_cast<btScalar>( lua_tostring( L, 2 ) );
-    const btScalar z = static_cast<btScalar>( lua_tostring( L, 3 ) );
+    const btScalar x = static_cast<btScalar>( lua_tonumber( L, 1 ) );
+    const btScalar y = static_cast<btScalar>( lua_tonumber( L, 2 ) );
+    const btScalar z = static_cast<btScalar>( lua_tonumber( L, 3 ) );
 
     ShapeSingle * s = new ShapeSingle();
     s->s = new btBoxShape( btVector3( x, y, z ) );
@@ -82,8 +83,8 @@ static int lua_btCapsuleCreate( lua_State * L )
 {
     const int top = lua_gettop( L );
     // Read parameters.
-    const btScalar r = static_cast<btScalar>( lua_tostring( L, 1 ) );
-    const btScalar h = static_cast<btScalar>( lua_tostring( L, 2 ) );
+    const btScalar r = static_cast<btScalar>( lua_tonumber( L, 1 ) );
+    const btScalar h = static_cast<btScalar>( lua_tonumber( L, 2 ) );
 
     ShapeSingle * s = new ShapeSingle();
     s->s = new btCapsuleShape( r, h );
@@ -112,6 +113,48 @@ static int lua_simpleShapeGc( lua_State * L )
 }
 
 
+
+
+
+
+
+
+
+static void simpleShapeCreateMeta( lua_State * L )
+{
+    const int top = lua_gettop( L );
+    luaL_newmetatable( L, SIMPLESHAPE_META_T_NAME );  // create new metatable for file handles
+    // file methods
+    lua_pushliteral( L, "__index" );
+    lua_pushvalue( L, -2 );  // push metatable
+    lua_rawset( L, -3 );     // metatable.__index = metatable
+
+    lua_pushliteral( L, "__gc" );
+    lua_pushcfunction( L, lua_simpleShapeGc );
+    lua_rawset( L, -3 );
+
+    // Clean stack.
+    lua_settop( L, top );
+}
+
+static const struct luaL_reg FUNCS[] =
+{
+    { "btSphere",  lua_btSphereCreate },
+    { "btBox",     lua_btBoxCreate },
+    { "btCapsule", btCapsuleCreate },
+
+    { 0, 0 },
+};
+
+
+
+
+
+int luaopen_btShapes( lua_State * L )
+{
+    simpleShapeCreateMeta( L );
+    luaL_register( L, LIB_NAME, FUNCS );
+}
 
 
 
---
+++ states_exploring/src/lua_collision_shapes.h
@@ -0,0 +1,12 @@
+
+#ifndef __LUA_COLLISION_SHAPES_H_
+#define __LUA_COLLISION_SHAPES_H_
+
+#include "lua.hpp"
+
+int luaopen_btShapes( lua_State * L );
+
+
+#endif
+
+
