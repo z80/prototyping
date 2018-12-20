02ad0b642599af118bb7e9abdbebf62a59eca40c
sbashkirov <sergey.bashkirov@sony.com>
2018-12-19 18:10:46 -0800

    Moved to the right place.

--- settings.cmake
+++ settings.cmake
@@ -1,6 +1,6 @@
 
-set( OGRE_HOME "~/projects/noise.git/ogre3d/install" )
-#set( OGRE_HOME "~/projects/ogre3d/ogre3d/install.debug" )
+#set( OGRE_HOME "~/projects/noise.git/ogre3d/install" )
+set( OGRE_HOME "~/projects/ogre3d/ogre3d/install.debug" )
 #set( OGRE_HOME "~/projects/noise.git/ogre3d/install.debug" )
 
 
