6f4b949f717a00369f9e98226bf11c4368b5f4f2
z80 <bashkirov.sergey@gmail.com>
2018-12-28 18:26:21 -0800

    a

--- states_exploring/src/designs/assemblies_manager.h
+++ states_exploring/src/designs/assemblies_manager.h
@@ -4,6 +4,11 @@
 
 #include "Ogre.h"
 
+namespace Config
+{
+    class ConfigReader;
+}
+
 namespace Entity
 {
 
@@ -12,6 +17,16 @@ class AssembliesManager
 public:
     AssembliesManager();
     ~AssembliesManager();
+
+    /// Load AssembliesManager's parameters from a config file.
+    bool load( Config::ConfigReader * cr );
+    /// Manually set parent, origin and orientation of assemblies to be created.
+    void setOrigin( EntityPlanet * planet,
+                    const Ogre::Vector3 & at,
+                    const Ogre::Quaternion & q=Ogre::Quaternion::IDENTITY );
+
+public:
+    std::vector<Assembly *> assemblies;
 };
 
 
