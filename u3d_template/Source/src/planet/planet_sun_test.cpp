8bb8e51658ef17fe74688aeb02935ae7a9c6f089
sbashkirov <sergey.bashkirov@sony.com>
2019-04-22 17:17:47 -0700

    Sun billboard material works.

--- u3d_template/Source/src/planet/planet_sun_test.cpp
+++ u3d_template/Source/src/planet/planet_sun_test.cpp
@@ -29,13 +29,19 @@ void PlanetSunTest::Start()
     {
         billboardNode = SharedPtr<Node>( dynamicsNode->CreateChild( "BillboardNode" ) );
         Node * s = billboardNode;
-        BillboardSet * bb = s->CreateComponent<BillboardSet>();
-        bb->SetNumBillboards( 1 );
+        BillboardSet * bbs = s->CreateComponent<BillboardSet>();
+        bbs->SetNumBillboards( 1 );
         ResourceCache * cache = GetSubsystem<ResourceCache>();
         Material * m = cache->GetResource<Material>( "Materials/SunTest.xml" );
-        bb->SetMaterial( m );
+        bbs->SetMaterial( m );
 
-        bb->Commit();
+        Billboard * bb = bbs->GetBillboard( 0 );
+        bb->position_  = Vector3::ZERO;
+        bb->size_      = Vector2::ONE * 10.0;
+        bb->rotation_  = 0.0;
+        bb->enabled_   = true;
+
+        bbs->Commit();
     }
 }
 
